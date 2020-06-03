6c371bf3fd72185b7952415c2ea0e591ef1e084d
KarpovaIryna <karpovai@outllok.com>
2020-06-03 21:33:31 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -12,15 +12,16 @@ function flyRocket(event) {
 	let xWalk = x - spaceCoords.left - space.clientLeft - rocket.clientWidth / 2;
 //Set rocket's coordinats in CSS
 console.log(xWalk, yWalk);
+
+//Set borders for space-field
+if(xWalk < 0) xWalk = 0;
+if(yWalk < 0) yWalk = 0;
+	if(xWalk + rocket.offsetWidth > space.clientWidth) xWalk = xWalk - rocket.offsetWidth / 2;	
+if(yWalk + rocket.offsetHeight > space.clientHeight) yWalk = yWalk - rocket.offsetHeight / 2;	
+
 rocket.style.left = `${xWalk}px`;
 rocket.style.top = `${yWalk}px`;
-//Set borders for space-field
-if(rocket.top < 0) rocket.top = 0;
-if(rocket.left < 0) rocket.left = 0;
-if(rocket.top > space.clientHeight) rocket.top = space.clientHeight;
-if(rocket.left > space.clientWidth) rocket.left = space.clientWidth;	
 };
 //Clicken
 space.addEventListener('click', flyRocket);
 })();
-
